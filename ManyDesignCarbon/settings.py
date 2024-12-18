010b4a0109aaf2ff6560f56a46493dbfcb1e306f
Lior Bakalo <103962173+LiorBak@users.noreply.github.com>
2024-12-18 21:00:08 +0200

    restore before db changes

--- ManyDesignCarbon/settings.py
+++ ManyDesignCarbon/settings.py
@@ -1,5 +1,5 @@
 from os import environ
-import dj_database_url
+#import dj_database_url
 
 SESSION_CONFIGS = [
      dict(
@@ -51,10 +51,9 @@ SECRET_KEY = '9727914704108'
 
 DEBUG = environ.get('DEBUG', 'False').lower() == 'true'
 
-AUTH_LEVEL = environ.get('OTREE_AUTH_LEVEL', None)
-
-
+#DATABASES = {
+#    'default': dj_database_url.config(default=environ.get('DATABASE_URL'))
+#}
 
 # if an app is included in SESSION_CONFIGS, you don't need to list it here
 INSTALLED_APPS = ['otree']
-
